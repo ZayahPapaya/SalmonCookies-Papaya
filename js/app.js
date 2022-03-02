fffd84dc9308de738a6f7f903d14dceb6896575a
Aiden <hyena.shenanigans@gmail.com>
2022-03-01 21:01:05 -0800

    Start of objects

--- js/app.js
+++ js/app.js
@@ -1,1 +1,17 @@
 'use strict';
+
+let hour = ['6.am', '7.am'];
+
+let  seattleObject = {
+  name: 'Seattle',
+  min: 23,
+  max: 65,
+  avg: 6.3,
+  dailyTotal: 0,
+  avgCookiesSoldEachHourArray:0,
+  getRandomCustomers: function(){
+    return Math.floor(Math.random)
+  }
+};
+
+console.log(seattleObject.getRandomCustomers);
